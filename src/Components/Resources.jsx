40eacf55bdc3767f1f6ebee4068eacac1f7909af
KristofferHoff <kristoffer.hoff@me.com>
2025-03-03 09:39:36 +0100

    oppdatering

---
+++ src/Components/Resources.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Resources() {
+  return (
+    <div>
+      <h1>Ressursene våre</h1>
+      <p>Her finner du informasjon om tilgjengelige ressurser.</p>
+      {/* Legg til ressurser, lister eller komponenter her */}
+    </div>
+  );
+}
+
+export default Resources;
